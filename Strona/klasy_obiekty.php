b93d08495c7faccf8d7fa5152550a0c6d9027336
BiveGpe <89411621+BiveGpe@users.noreply.github.com>
2021-09-01 02:53:37 +0200

    początek klas i obiektów

---
+++ Strona/klasy_obiekty.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Klasy i Obiekty</title>
+</head>
+<body>
+    <h1>Klasy i Obiekty</h1>
+
+    <a href="index.html"><br>wróć</a>
+</body>
+</html>
